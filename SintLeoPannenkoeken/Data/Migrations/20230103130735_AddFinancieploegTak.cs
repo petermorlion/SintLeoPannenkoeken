3b4357e2d861bce9fcdf33b7ba5ef5e8bdc050db
Peter Morlion <peter.morlion@gmail.com>
2023-01-03 14:10:48 +0100

    Add Financieploeg

---
+++ SintLeoPannenkoeken/Data/Migrations/20230103130735_AddFinancieploegTak.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SintLeoPannenkoeken.Data.Migrations
+{
+    public partial class AddFinancieploegTak : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertTak("Financieploeg");
+            migrationBuilder.SetTakAfkorting("Financieploeg", "FP");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            return migrationBuilder.Sql($"DELETE FROM Takken WHERE [Naam] = '{naam}';");
+        }
+    }
+}
