3d86e9e7ec4e0c27acc6ab492440c56fe25ccc1a
Peter Morlion <peter.morlion@gmail.com>
2023-02-21 19:32:40 +0100

    Partially refactor create/edit bestelling but store WIP so we can focus on drivers first

---
+++ SintLeoPannenkoeken/ViewModels/Bestellingen/BestellingFormViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SintLeoPannenkoeken.ViewModels.Bestellingen
+{
+    public class BestellingFormViewModel
+    {
+        public string FormId { get; set; }
+        public IndexViewModel IndexViewModel { get; set; }
+    }
+}
