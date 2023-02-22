c734f423317ff8ef4c150e4b1dc622208f6643c8
Peter Morlion <peter.morlion@gmail.com>
2023-02-22 22:04:20 +0100

    Remove ronde controllers

--- SintLeoPannenkoeken/ViewModels/Rondes/CreateRondeViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SintLeoPannenkoeken.ViewModels.Rondes
-{
-    public class CreateRondeViewModel
-    {
-        public int BestuurderId { get; set; }
-        public int ZoneId { get; set; }
-    }
-}
