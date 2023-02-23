ccf7f0c1b3f74112466aafb2a63346af97067799
Peter Morlion <peter.morlion@gmail.com>
2023-02-23 18:19:15 +0100

    Add rapporten, doesn't work yet

---
+++ SintLeoPannenkoeken/Controllers/RapportenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SintLeoPannenkoeken.Controllers
+{
+    public class RapportenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
