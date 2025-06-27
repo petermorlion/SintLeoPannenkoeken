6dbd2831a82266fb8c626b2e6564c77fdb269b23
Peter Morlion <peter.morlion@gmail.com>
2025-06-27 10:01:50 +0200

    Add MudBlazor

--- SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Program.cs
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Program.cs
@@ -1,11 +1,14 @@
-using SintLeoPannenkoeken.Blazor.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using SintLeoPannenkoeken.Blazor.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
--- SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor/Program.cs
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
 using SintLeoPannenkoeken.Blazor.Client.Pages;
 using SintLeoPannenkoeken.Blazor.Components;
 using SintLeoPannenkoeken.Blazor.Components.Account;
@@ -43,6 +44,8 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
