b04934a5672edc7b399b694f3c5e98e257bfb231
Peter Morlion <peter.morlion@gmail.com>
2025-06-27 11:00:47 +0200

    WIP

--- SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Program.cs
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using SintLeoPannenkoeken.Blazor.Client;
+using SintLeoPannenkoeken.Blazor.Client.Server;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -10,5 +11,7 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
 builder.Services.AddMudServices();
+
+builder.Services.AddHttpClient<IServerHttpClient, ServerHttpClient>();
 
 await builder.Build().RunAsync();
