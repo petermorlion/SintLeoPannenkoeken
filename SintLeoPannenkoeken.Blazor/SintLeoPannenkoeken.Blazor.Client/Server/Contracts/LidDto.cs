f5f5b02637c9d4c326247ed42bbeb71b58d5a5f0
Peter Morlion <peter.morlion@gmail.com>
2025-07-09 12:03:26 +0200

    Add authN/Z back

---
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Server/Contracts/LidDto.cs
@@ -0,0 +1,20 @@
+﻿namespace SintLeoPannenkoeken.Blazor.Client.Server.Contracts
+{
+    public class LidDto
+    {
+        public LidDto(int id, string achternaam, string voornaam, string functie, string takNaam)
+        {
+            Id = id;
+            Achternaam = achternaam;
+            Voornaam = voornaam;
+            Functie = functie;
+            TakNaam = takNaam;
+        }
+
+        public int Id { get; init; }
+        public string Achternaam { get; init; }
+        public string Voornaam { get; init; }
+        public string Functie { get; init; }
+        public string TakNaam { get; init; }
+    }
+}
---
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor/Controllers/LedenController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SintLeoPannenkoeken.Blazor.Client.Server.Contracts;
+using SintLeoPannenkoeken.Blazor.Data;
+
+namespace SintLeoPannenkoeken.Blazor.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
+    [ApiController]
+    public class LedenController : ControllerBase
+    {
+        private readonly ILogger<LedenController> _logger;
+        private readonly ApplicationDbContext _dbContext;
+
+        public LedenController(ILogger<LedenController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            var leden = _dbContext
+                .Leden
+                .Include(lid => lid.Tak)
+                .OrderBy(lid => lid.Achternaam)
+                .ToList();
+
+            var lidDtos = leden == null 
+                ? new List<LidDto>() 
+                : leden.Select(lid => new LidDto(
+                    lid.Id,
+                    lid.Voornaam,
+                    lid.Achternaam,
+                    lid.Functie,
+                    lid.Tak?.Naam ?? "Onbekend"
+                )).ToList();
+
+            return Ok(lidDtos);
+        }
+    }
+}
