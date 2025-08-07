f1be64c83142f29a44ad26ac943cae6e05b557f9
Peter Morlion <peter.morlion@gmail.com>
2025-08-07 13:22:40 +0200

    RefactÂor MasterDetailPage to contain more common logic

---
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor/Controllers/TakkenController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SintLeoPannenkoeken.Blazor.Client.Server;
+
+namespace SintLeoPannenkoeken.Blazor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TakkenController : ControllerBase
+    {
+        private readonly ILogger<TakkenController> _logger;
+        private readonly IServerData _serverData;
+
+        public TakkenController(ILogger<TakkenController> logger, IServerData serverData)
+        {
+            _logger = logger;
+            _serverData = serverData;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var takDtos = await _serverData.GetTakken();
+            return Ok(takDtos);
+        }
+    }
+}
