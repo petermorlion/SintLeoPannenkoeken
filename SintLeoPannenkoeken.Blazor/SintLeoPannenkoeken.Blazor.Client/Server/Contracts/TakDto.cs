faa877c8f129cd30e082954bf6fafe1497f0ee20
Peter Morlion <peter.morlion@gmail.com>
2025-08-07 13:22:40 +0200

    RefactÂor MasterDetailPage to contain more common logic

---
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Server/Contracts/TakDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SintLeoPannenkoeken.Blazor.Client.Server.Contracts
+{
+    public class TakDto
+    {
+        public TakDto(int id, string naam)
+        {
+            Id = id;
+            Naam = naam;
+        }
+
+        public int Id { get; init; }
+        public string Naam { get; init; }
+    }
+}
