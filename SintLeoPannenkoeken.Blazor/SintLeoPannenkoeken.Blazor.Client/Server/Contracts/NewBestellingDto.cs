042e1358038612e6718ca89da46bda2b86f3e902
Peter Morlion <peter.morlion@gmail.com>
2025-08-20 15:28:58 +0200

    Store bestelling

--- SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Server/Contracts/NewBestellingDto.cs
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Server/Contracts/NewBestellingDto.cs
@@ -5,14 +5,14 @@
         public int Id { get; set; }
         public int BestellingNummer { get; set; }
         public string Naam { get; set; }
-        public string? Telefoon { get; set; }
+        public string? Telefoon { get; set; } = "";
         public int AantalPakken { get; set; }
-        public string? Opmerkingen { get; set; }
+        public string? Opmerkingen { get; set; } = "";
         public bool Betaald { get; set; }
         public bool Geleverd { get; set; }
         public int StraatId { get; set; }
-        public string Nummer { get; set; }
-        public string Bus { get; set; }
+        public string? Nummer { get; set; } = "";
+        public string? Bus { get; set; } = "";
         public int LidId { get; set; }
         public int ScoutsjaarId { get; set; }
     }
