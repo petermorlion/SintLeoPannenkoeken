195c0d331b20bafee5a6ea9e5d5fc8ef39739ab2
Peter Morlion <peter.morlion@gmail.com>
2025-08-25 15:58:24 +0200

    Add CRUD for chauffeurs

--- SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor/Controllers/ChauffeursController.cs
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor/Controllers/ChauffeursController.cs
@@ -2,11 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using SintLeoPannenkoeken.Blazor.Client.Auth;
 using SintLeoPannenkoeken.Blazor.Client.Server;
+using SintLeoPannenkoeken.Blazor.Client.Server.Contracts;
 
 namespace SintLeoPannenkoeken.Blazor.Controllers
 {
     [Route("api/[controller]")]
-    [Authorize(Roles = $"{Roles.RolesForGebruikers}")]
+    [Authorize(Roles = $"{Roles.RolesForChauffeurs}")]
     [ApiController]
     public class ChauffeursController : ControllerBase
     {
@@ -25,6 +26,28 @@ namespace SintLeoPannenkoeken.Blazor.Controllers
         {
             var chauffeurDtos = await _serverData.GetChauffeurs();
             return Ok(chauffeurDtos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateChauffeur([FromBody] ChauffeurDto chauffeurDto)
+        {
+            var result = await _serverData.CreateChauffeur(chauffeurDto);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateChauffeur([FromBody] ChauffeurDto chauffeurDto)
+        {
+            await _serverData.UpdateChauffeur(chauffeurDto);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> UpdateChauffeur(int id)
+        {
+            await _serverData.DeleteChauffeur(id);
+            return Ok();
         }
     }
 }
