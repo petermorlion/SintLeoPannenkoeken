55362710b83e75b262cbcaecf5646e34f901be36
Peter Morlion <peter.morlion@gmail.com>
2025-11-10 10:27:42 +0100

    Show active page

---
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Pages/Hrefs.cs
@@ -0,0 +1,19 @@
+﻿namespace SintLeoPannenkoeken.Blazor.Client.Pages
+{
+    public class Hrefs
+    {
+        public const string Bestellingen = "verkoop/bestellingen";
+        public const string Chauffeurs = "beheer/chauffeurs";
+        public const string Rondes = "verdeling/rondes";
+        public const string Streefcijfers = "beheer/streefcijfers";
+        public const string VerkoopPerTak = "rapporten/verkooppertak";
+        public const string VerkoopPerLid = "rapporten/verkoopperlid";
+        public const string IngegevenTotalen = "rapporten/ingegeventotalen";
+        public const string Gebruikers = "beheer/gebruikers";
+        public const string Leden = "beheer/leden";
+        public const string Scoutsjaren = "beheer/scoutsjaren";
+        public const string Straten = "beheer/straten";
+        public const string Account = "account/manage";
+        public const string Login = "account/login";
+    }
+}
