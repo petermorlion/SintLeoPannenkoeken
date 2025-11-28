1257c65779e381f7a9df272120a8df9377667906
Peter Morlion <peter.morlion@gmail.com>
2025-11-28 13:40:40 +0100

    Add missing files

---
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Server/Contracts/Rapporten/LidVerkoopDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SintLeoPannenkoeken.Blazor.Client.Server.Contracts.Rapporten
+{
+    public class LidVerkoopDto
+    {
+        public int LidId { get; set; }
+        public string Voornaam { get; set; } = string.Empty;
+        public string Achternaam { get; set; } = string.Empty;
+        public string TakNaam { get; set; } = string.Empty;
+        public int AantalPakkenVerkocht { get; set; }
+    }
+}
---
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/Server/Contracts/Rapporten/VerkoopPerLidDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SintLeoPannenkoeken.Blazor.Client.Server.Contracts.Rapporten
+{
+    public class VerkoopPerLidDto
+    {
+        public int ScoutsjaarBegin { get; set; }
+        public List<LidVerkoopDto> LidVerkopen { get; set; } = new List<LidVerkoopDto>();
+    }
+}
