8da1440c36d076e55867d84353d173200e18c3d4
Peter Morlion <peter.morlion@gmail.com>
2026-01-26 16:57:40 +0100

    Style home

---
+++ SintLeoPannenkoeken.Blazor/SintLeoPannenkoeken.Blazor.Client/wwwroot/homeCharts.js
@@ -0,0 +1,38 @@
+﻿window.homeCharts = {
+    loadIngaveTotalen: (data) => {
+        const ingaveTotalenChart = document.getElementById('ingaveTotalenChart');
+        console.log('chart', ingaveTotalenChart);
+        new Chart(
+            ingaveTotalenChart,
+            {
+                type: 'line',
+                data: {
+                    labels: data.ingaveTotalen.map((row) => ''),
+                    datasets: [
+                        {
+                            label: 'Verkoop',
+                            data: data.ingaveTotalen.map(row => row.oplopendTotaal),
+                            backgroundColor: '#9dc3ca',
+                            fill: true,
+                            tension: 0.4,
+                            cubicInterpolationMode: 'monotone',
+                        },
+                    ]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            min: 0,
+                            max: data.ingaveTotalen.at(-1)?.oplopendTotaal + 20 ?? 100
+                        }
+                    },
+                    elements: {
+                        point: {
+                            pointStyle: false
+                        }
+                    }
+                }
+            }
+        );
+    }
+}
